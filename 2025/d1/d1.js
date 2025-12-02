89ed734a3518e800f66da3e7d18510768c7e5ca5
Kevin Dillon <kdillo893@gmail.com>
2025-12-02 17:19:22 -0600

    part2 complete, relatively inefficient

--- 2025/d1/d1.js
+++ 2025/d1/d1.js
@@ -6,40 +6,60 @@ function main() {
 
   console.log("processing", process.argv[2]);
 
-    (async () => {
-      try {
-        const file = await fs.open(process.argv[2]);
+  (async () => {
+    try {
+      const file = await fs.open(process.argv[2]);
 
-        //position starts at 50
-        let position = 50;
-        let pw = 0;
+      //position starts at 50
+      let position = 50;
+      let pw = 0;
 
-        for await (const line of file.readLines()) {
-          //first character is L/R, this determines add/sub
-          // L = -1, R = +1
-          const lr = 1 * (((line.charAt(0) === 'R') * 2) - 1)
+      for await (const line of file.readLines()) {
 
-          //the value is int at index 1 of the line
-          const val = Number(line.substring(1));
+        //dial spin: L = -1, R = +1
+        const lr = 1 * (((line.charAt(0) === 'R') * 2) - 1)
+        let val = Number(line.substring(1));
 
-          position = position + (lr * val);
+        //part2 method:
+        //  a. if number is greater than 100, mod it 100 and
+        //      add 100s place to pw
+        const fulls = Math.floor(val / 100);
+        pw += fulls;
+        val = val % 100;
 
-          //position fixing: add 100 mod 100, lets negatives go in
-          //  the normal wheel
-          position = (position + 100) % 100;
-
-          if (!position) {
-            pw++;
-          }
-
+        const lastPos = position;
+        position = position + (lr * val);
+        //  b. if going from positive to negative, give extra point
+        //      ensure not from 0
+        const leftFlip = position < 0 && lastPos != 0;
+        if (leftFlip) {
+          pw++;
         }
 
-        console.log("pw=", pw);
+        //  c. if exceeding 100, extra point
+        const rFlip = position > 100;
+        if (rFlip) {
+          pw++;
+        }
 
-      } catch (er) {
-        console.error(er);
+        //position fixing: add 100 mod 100, lets negatives go in
+        //  the normal wheel
+        position = (position + 100) % 100;
+
+        //1. "end on 0"
+        if (!position) {
+          pw++;
+        }
+
+        console.log({ line, position, l: Number(leftFlip), r: Number(rFlip), fulls, pw });
       }
-    })();
+
+      console.log("pw=", pw);
+
+    } catch (er) {
+      console.error(er);
+    }
+  })();
 
 }
 
