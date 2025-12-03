77773ec7683207a4f1b48b19bb5a00d444ca2ce1
Kevin Dillon <kdillo893@gmail.com>
2025-12-03 17:32:10 -0600

    timing and minor optimizations... why is argParser so slow?

--- 2025/d3/d3.py
+++ 2025/d3/d3.py
@@ -1,43 +1,62 @@
 import argparse
+import time
+
+
+def joltage12(line):
+    """
+    take a line string with numeric characters and return the int value
+    of "joltage" from the highest possible 12-digit value
+    """
+    pass
 
 
 def lineJoltage(line):
     """
     take a line string with numeric characters and return the int value
-    of "joltage" from the highest possible value left-to-right
+    of "joltage" from the highest possible 2-digit value left-to-right
+    Optimizations:
+        1. ord is faster than int translation
+        2. ... I think there's a way to seek quicker
     """
 
-    if len(line) <= 2:
+    length = len(line)
+
+    if length <= 2:
         return int(line)
 
     left = 0
     leftMax = 0
 
     # seek the highest left, then right
-    for lidx in range(0, len(line) - 1):
-        int_l = int(line[lidx])
+    for lidx in range(0, length - 1):
+        int_l = ord(line[lidx])
         if int_l > leftMax:
             left = lidx
             leftMax = int_l
-            pass
+
+            if leftMax == 9:
+                break
         pass
 
     right = left+1
     rightMax = 0
-    for ridx in range(right, len(line)):
-        int_r = int(line[ridx])
+    for ridx in range(right, length):
+        int_r = ord(line[ridx])
         if int_r > rightMax:
             right = ridx
             rightMax = int_r
-            pass
+
+            if rightMax == 9:
+                break
         pass
 
     result = int(line[left] + line[right])
-    print(line, result)
+    # print(line, result)
     return result
 
 
 def main():
+    argParseTime = time.process_time_ns()
     argParser = argparse.ArgumentParser(
         prog="Advent of Code 2025: Day 3",
         usage="python d3.py [filename]",
@@ -46,16 +65,22 @@ def main():
     argParser.add_argument('filename')
     args = argParser.parse_args()
 
+    argParseTime = time.process_time_ns() - argParseTime
+
+    fileSplitTime = time.process_time_ns()
     # parse file in first arg
     file = open(args.filename, 'r')
-
     lines = file.read().splitlines(False)
+    fileSplitTime = time.process_time_ns() - fileSplitTime
 
+    joltCalcTime = time.process_time_ns()
     totalJoltage = 0
     for line in lines:
         totalJoltage += lineJoltage(line)
+    joltCalcTime = time.process_time_ns() - joltCalcTime
 
     print("totalJoltage=", totalJoltage)
+    print(f"apt={argParseTime}ns, fst={fileSplitTime}ns, jct={joltCalcTime}ns")
     return
 
 
